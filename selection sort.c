e7412cfa3bc757ffbbc6e49836172f352446c3d3
WooJyu <66297198+Jday4612@users.noreply.github.com>
2023-09-20 12:42:48 +0900

    Update selection sort.c

--- selection sort.c
+++ selection sort.c
@@ -2,52 +2,56 @@
 #include <stdio.h>
 
 #define MAX_SIZE 1000
-void selectionSort(int a[], int n);
 
-int main() {
+void swap(int* a, int* b);
+void insertionSort(int a[], int n);
+
+int main()
+{
 	int numTestCases;
-
 	scanf("%d", &numTestCases);
-	for (int i = 0; i < numTestCases; ++i) {
+
+	for (int i = 0; i < numTestCases; ++i)
+	{
 		int num;
 		int a[MAX_SIZE];
 
 		scanf("%d", &num);
+
 		for (int j = 0; j < num; j++)
 			scanf("%d", &a[j]);
 
-		selectionSort(a, num);
+		insertionSort(a, num);
 		printf("\n");
 	}
 
 	return 0;
 }
 
-void swap(int* a, int* b)
-{
+void swap(int* a, int* b) {
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
-/* Selection Sort ÇÔ¼ö */
-void selectionSort(int a[], int n) {
-	int countCmpOps = 0; // ºñ±³ ¿¬»êÀÚ ½ÇÇà È½¼ö
-	int countSwaps = 0; // swap ÇÔ¼ö ½ÇÇà È½¼ö
+/* Insertion Sort í•¨ìˆ˜ */
+void insertionSort(int a[], int n)
+{
+	int countCmpOps = 0; // ë¹„êµ ì—°ì‚°ìž ì‹¤í–‰ íšŸìˆ˜
+	int countSwaps = 0; // swap í•¨ìˆ˜ ì‹¤í–‰ íšŸìˆ˜
 
-	// selection sort ¾Ë°í¸®Áò ±¸Çö
-	for (int i = 0; i < n - 1; i++) {
-		int jMin = i;
-
-		for (int j = i + 1; j < n; j++) {
-			if (a[jMin] > a[j])
-				jMin = j;
+	// insertion sort ì•Œê³ ë¦¬ì¦˜ êµ¬í˜„
+	for (int i = 1; i < n; i++) {
+		for (int j = i; 0 < j; j--) {
 			countCmpOps++;
-		}
 
-		if (jMin != i) {
-			swap(&a[jMin], &a[i]);
-			countSwaps++;
+			if (a[j - 1] > a[j]) {
+				swap(&a[j - 1], &a[j]);
+				countSwaps++;
+			}
+			else {
+				break;
+			}
 		}
 	}
 
