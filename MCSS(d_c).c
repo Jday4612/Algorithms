76de1fa23e60c7a53e59dfea47ac3b30edd4297e
WooJyu <66297198+Jday4612@users.noreply.github.com>
2023-10-05 22:52:27 +0900

    Update MCSS(d_c).c

--- MCSS(d_c).c
+++ MCSS(d_c).c
@@ -1,67 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX_SIZE 100
 int maxSubsequenceSum(int a[], int start, int end);
 
-int main() {
-    int numTestCases;
+int main()
+{
+	int numTestCases;
+	scanf("%d", &numTestCases);
 
-    scanf("%d", &numTestCases);
-    for (int i = 0; i < numTestCases; ++i) {
-        int n, result;
-        int a[MAX_SIZE];
+	while (numTestCases--) {
+		int n, f[100], start = -1, end = -1;
 
-        scanf("%d", &n);
+		scanf("%d", &n);
 
-        for (int j = 0; j < n; j++)
-            scanf("%d", &a[j]);
+		for (int i = 0; i < n; i++)
+			scanf("%d", &f[i]);
 
-        result = maxSubsequenceSum(a, 0, n - 1);
+		printf("%d\n", maxSubsequenceSum(f, 0, n - 1));
+	}
 
-        if (result < 0)
-            printf("0\n");
-        else
-            printf("%d\n", result);
-    }
-    return 0;
+	return 0;
 }
 
-/* Maximum Contiguous Subsequence Sum 함수 */
+int max(int a, int b) {
+	return a >= b ? a : b;
+}
+
 int maxSubsequenceSum(int a[], int start, int end) {
-    int half, left, right, mid, check;
-    int maxLeft, maxRight, thisSum;
+	int left_maxSum = 0, right_maxSum = 0, half, biasedSum, maxSum;
 
-    if (start == end)
-        return a[start];
-    else {
-        //Divide : n 개의 정수배열을 크기가 각각 n/2 개인 부분배열로 나눈다.
-        half = (start + end) / 2;
+	if (start == end)
+		return a[start];
+	else {
+		half = (start + end) / 2;
 
-        //Conquer :크기가 각각 n / 2 개인 부분배열에 대하여 recursive 하게 문제를 해결한다.
-        maxLeft = a[half] - 1;
-        thisSum = 0;
-        for (int i = half; start <= i; i--) {
-            thisSum += a[i];
-            maxLeft = thisSum > maxLeft ? thisSum : maxLeft;
-        }
+		int left_thisSum = 0;
+		int right_thisSum = 0;
 
-        maxRight = a[half + 1] - 1;
-        thisSum = 0;
-        for (int i = half + 1; i <= end; i++) {
-            thisSum += a[i];
-            maxRight = thisSum > maxRight ? thisSum : maxRight;
-        }
-        
-        mid = maxLeft + maxRight;
-        left = maxSubsequenceSum(a, start, half);
-        right = maxSubsequenceSum(a, half + 1, end);
+		for (int i = half; start <= i; i--) {
+			left_thisSum += a[i];
+			left_maxSum = max(left_maxSum, left_thisSum);
+		}
 
-        //Combine : "Conquer" 단계에서 계산한 값과 다음과 같이 연속된 구간이 중앙을 걸쳐서\
-        존재할 수 있으므로 이런 경우도 해결하여야 한다.이를 위해서는 중앙을 중심으로 왼쪽으로\
-        연속적으로 최대가 되는 값과 중앙을 중심으로 오른쪽으로 연속적으로 최대가 되는 값을 구하여 합한다.
-        check = left > right ? left : right; // 왼쪽 또는 오른쪽에 있는지 확인
-        
-        return mid > check ? mid : check;
-    }
+		for (int i = half + 1; i <= end; i++) {
+			right_thisSum += a[i];
+			right_maxSum = max(right_maxSum, right_thisSum);
+		}
+
+		biasedSum = max(maxSubsequenceSum(a, start, half), maxSubsequenceSum(a, half + 1, end));
+		maxSum = max(biasedSum, left_maxSum + right_maxSum);
+
+		return maxSum;
+	}
 }
