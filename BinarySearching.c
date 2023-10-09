93896e5dad0ec950d64c1cf6e6f36aec2df2b348
WooJyu <66297198+Jday4612@users.noreply.github.com>
2023-10-09 17:15:58 +0900

    Update BinarySearching.c

--- BinarySearching.c
+++ BinarySearching.c
@@ -1,45 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX_SIZE 100
-
 int binarySearch(int a[], int left, int right, int value);
 
-int main() {
-    int numTestCases;
+int main()
+{
+	int numTestCases;
+	scanf("%d", &numTestCases);
 
-    scanf("%d", &numTestCases);
-    for (int i = 0; i < numTestCases; ++i) {
-        int n, k;
-        int a[MAX_SIZE];
+	while (numTestCases--) {
+		int n, k, f[100];
 
-        scanf("%d", &n);
-        scanf("%d", &k);
+		scanf("%d %d", &n, &k);
 
-        for (int j = 0; j < n; j++)
-            scanf("%d", &a[j]);
+		for (int i = 0; i < n; i++)
+			scanf("%d", &f[i]);
 
-        printf("%d\n", binarySearch(a, 0, n - 1, k));
-    }
-    return 0;
+		printf("%d\n", binarySearch(f, 0, n - 1, k));
+	}
+
+	return 0;
 }
 
-/* binarySearch ÇÔ¼ö */
 int binarySearch(int a[], int left, int right, int value) {
-    int mid;
+	int mid;
 
-    if (left > right)
-        return -1;
-    else {
-        //Divide
-        mid = (left + right) / 2;
+	if (left > right)
+		return -1;
+	else {
+		mid = (left + right) / 2;
 
-        //Conquer
-        if (a[mid] == value)
-            return mid;
-        else if (a[mid] > value)
-            return binarySearch(a, left, mid - 1, value);
-        else
-            return binarySearch(a, mid + 1, right, value);
-    }
+		if (a[mid] == value)
+			return mid;
+		else if (a[mid] > value)
+			return binarySearch(a, left, mid - 1, value);
+		else
+			return binarySearch(a, mid + 1, right, value);
+	}
 }
