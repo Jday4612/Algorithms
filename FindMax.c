d2a1500743ada86b54994cfb1f0dcd8c819a19de
WooJyu <66297198+Jday4612@users.noreply.github.com>
2023-10-09 17:02:08 +0900

    Update FindMax.c

--- FindMax.c
+++ FindMax.c
@@ -3,46 +3,35 @@
 
 #define MAX_SIZE 100
 #define MAX(a,b) ((a)>(b)?(a):(b))
-
 int recurMax(int a[], int left, int right);
 
-int main() {
-    int numTestCases;
+int main()
+{
+	int numTestCases;
+	scanf("%d", &numTestCases);
 
-    scanf("%d", &numTestCases);
-    for (int i = 0; i < numTestCases; ++i) {
-        int n, result;
-        int a[MAX_SIZE];
+	while (numTestCases--) {
+		int n, f[100];
 
-        scanf("%d", &n);
+		scanf("%d", &n);
 
-        for (int j = 0; j < n; j++)
-            scanf("%d", &a[j]);
+		for (int i = 0; i < n; i++)
+			scanf("%d", &f[i]);
 
-        result = recurMax(a, 0, n - 1);
+		printf("%d\n", recurMax(f, 0, n - 1));
+	}
 
-        printf("%d\n", result);
-    }
-    return 0;
+	return 0;
 }
 
-/* Find Max ÇÔ¼ö */
-int recurMax(int a[], int left, int right)
-{
-    int half, maxLeft, maxRight;
+int recurMax(int a[], int left, int right) {
+	int half;
 
-    if (left == right)
-        return a[left];
-    else
-    {
-        //Divide
-        half = (left + right) / 2;
-
-        //Conquer
-        maxLeft = recurMax(a, left, half);
-        maxRight = recurMax(a, half + 1, right);
-
-        //Combine
-        return MAX(maxLeft, maxRight);
-    }
+	if (left == right)
+		return a[left];
+	else {
+		half = (left + right) / 2;
+		return MAX(recurMax(a, left, half),
+			recurMax(a, half + 1, right));
+	}
 }
